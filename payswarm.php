f793c9a07fd98cb1122dc7c2e20a6b19fe2a0d84
Dave Longley <dlongley@digitalbazaar.com>
2012-09-19 11:42:35 -0400

    Add payswarm session widget.

--- payswarm.php
+++ payswarm.php
@@ -36,9 +36,11 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 require_once('payswarm-config.inc');
 require_once('payswarm-admin.inc');
 require_once('payswarm-article.inc');
+require_once('payswarm-session-widget.inc');
 
 // handle install/uninstall PaySwarm database
 register_activation_hook(__FILE__, 'payswarm_install_database');
+register_activation_hook(__FILE__, 'payswarm_install_widgets');
 register_deactivation_hook(__FILE__, 'payswarm_uninstall_database');
 
 // add actions associated with the WordPress processing
@@ -53,5 +55,11 @@ add_action('admin_enqueue_scripts', 'payswarm_enqueue_scripts');
 
 // add filters for text that the PaySwarm plugin will modify
 add_filter('the_content', 'payswarm_filter_paid_content');
+
+// register widgets
+add_action('widgets_init', 'payswarm_init_widgets');
+function payswarm_init_widgets() {
+  register_widget('PaySwarmSessionWidget');
+}
 
 /* end of file, omit ?> */
